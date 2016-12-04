567bf1154bb3d8e48a93916be59179bade1b4316
Justin Chan <jcsl76@yahoo.com>
2016-12-04 04:54:04 +0000

    Initial commit. Method to calculate days between two dates.

---
+++ leap.py
@@ -0,0 +1,17 @@
+import unittest
+from datetime import date
+
+#Method to calculate number of days between 2 dates
+def DaysBetweenDates(startdate, enddate):
+    return (enddate-startdate).days
+
+#unit tests
+class MyTest(unittest.TestCase):
+    def test(self):
+        startdate = date(2008,8,1)
+        enddate = date(2008,12,1)
+        self.assertEqual(DaysBetweenDates(startdate, enddate), 122)
+
+
+if __name__ == '__main__':
+    unittest.main()
