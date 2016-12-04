18923f70fb419edc0b938b5b4572c2d6b2c29f57
Justin Chan <jcsl76@yahoo.com>
2016-12-04 06:05:44 +0000

    Read CSV file with one record

--- leap.py
+++ leap.py
@@ -3,7 +3,9 @@
 #       The author does not take any liability or responsibility with the use of the code.
 
 import unittest
+import csv
 from datetime import date
+
 
 #Method to calculate number of days between 2 dates
 def DaysBetweenDates(startdate, enddate):
@@ -15,9 +17,22 @@ def DaysBetweenDates(startdate, enddate):
 def ExposureByLives(startdate, enddate):
     return DaysBetweenDates(startdate, enddate)/365.0
 
+#Read record from filename provided. CSV format.
+def ReadRecord(filename):
+    f = open(filename, 'rt')
+    result=[]
+    try:
+        reader = csv.reader(f)
+        for row in reader:
+            #print(row)
+            result.append(row)
+    finally:
+        f.close() 
+    return result
+
 
 #unit tests
-class MyTest(unittest.TestCase):
+class DateTest(unittest.TestCase):
     def test(self):
         #test dates, same year
         startdate = date(2008,8,1)
@@ -29,7 +44,14 @@ class MyTest(unittest.TestCase):
         startdate = date(2008,8,1)
         enddate = date(2009,12,1)
         self.assertEqual(DaysBetweenDates(startdate, enddate), 487)
-        
+
+class CSVTest(unittest.TestCase):
+    #test csv file reading
+    def test(self):
+        expectedresult = ReadRecord('test.csv')
+        self.assertEqual(expectedresult[0][0],"PolicyNumber")
+        self.assertEqual(expectedresult[1][0],"A001")
+
 
 if __name__ == '__main__':
     unittest.main()
