272edbba2e8d0b14f6e191a7809d10f9e211e2e3
Justin Chan <jcsl76@yahoo.com>
2016-12-04 05:26:06 +0000

    Added ExposureByLives

--- leap.py
+++ leap.py
@@ -9,13 +9,27 @@ from datetime import date
 def DaysBetweenDates(startdate, enddate):
     return (enddate-startdate).days
 
+#Method to calculate exposure by lives
+#Basically, number of days between to dates, divided by days in a year
+#TODO: Adjust for leap year
+def ExposureByLives(startdate, enddate):
+    return DaysBetweenDates(startdate, enddate)/365.0
+
+
 #unit tests
 class MyTest(unittest.TestCase):
     def test(self):
+        #test dates, same year
         startdate = date(2008,8,1)
         enddate = date(2008,12,1)
         self.assertEqual(DaysBetweenDates(startdate, enddate), 122)
+        self.assertEqual(ExposureByLives(startdate,enddate), 122/365.0)
 
+        #test dates, different year
+        startdate = date(2008,8,1)
+        enddate = date(2009,12,1)
+        self.assertEqual(DaysBetweenDates(startdate, enddate), 487)
+        
 
 if __name__ == '__main__':
     unittest.main()
